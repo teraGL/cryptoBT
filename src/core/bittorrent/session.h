fc683a4e3bda7afe5aaa26ae364609182fba8c8c
Vlad Vasilkovsky <duglaz5endt0bytes@tutanota.de>
2019-06-08 18:31:16 +0300

    Add tabWidget, uncomment Session class

--- src/core/bittorrent/session.h
+++ src/core/bittorrent/session.h
@@ -1,63 +1,64 @@
-//#ifndef SESSION_H
-//#define SESSION_H
+#ifndef SESSION_H
+#define SESSION_H
 
-//#include <QPointer>
-//#include <QFile>
+#include <QPointer>
+#include <QFile>
 
-//#include "addtorrentparams.h"
-//#include "torrentinfo.h"
-//#include "sessionstatus.h"
+#include "addtorrentparams.h"
+#include "torrentinfo.h"
+#include "sessionstatus.h"
+
+#include <libtorrent/session.hpp>
+
+namespace BitTorrent
+{
+
+class AddTorrentParams;
+class SessionStatus;
+
+class Session : public QObject
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(Session)
+
+public:
+    static void initInstance();
+    static void freeInstance();
+    static Session* instance();
+
+    QString defaultSavePath() const;
+    void setDefaultSavePath(QString path);
+
+    bool addTorrent(const QString &source, const AddTorrentParams &params = AddTorrentParams());
+    bool addTorrent(const TorrentInfo &torrentInfo, const AddTorrentParams &params = AddTorrentParams());
+    bool deleteTorrent(const QString &hash, bool deleteLocalFiles = false);
+
+    int downloadSpeedLimit() const;
+    void setDownloadSpeedLimit(int limit);
+    int uploadSpeedLimit() const;
+    void setUploadSpeedLimit(int limit);
+
+    bool hasActiveTorrents() const;
+    bool hasUnfinishedTorrents() const;
+    bool hasRunningSeed() const;
 
 
-//namespace BitTorrent
-//{
+private:
+    explicit Session(QObject* parent = nullptr);
+    ~Session();
 
-//class AddTorrentParams;
-//class SessionStatus;
+    void initResumeFolder();
 
-//class Session : public QObject
-//{
-//    Q_OBJECT
-//    Q_DISABLE_COPY(Session)
+    // BitTorrent
+    libtorrent::session* native_session_;
 
-//public:
-//    static void initInstance();
-//    static void freeInstance();
-//    static Session *instance();
+    QString resume_folder_path_;
 
-//    QString defaultSavePath() const;
-//    void setDefaultSavePath(QString path);
+    SessionStatus status_;
 
-//    bool addTorrent(const QString &source, const AddTorrentParams &params = AddTorrentParams());
-//    bool addTorrent(const TorrentInfo &torrentInfo, const AddTorrentParams &params = AddTorrentParams());
-//    bool deleteTorrent(const QString &hash, bool deleteLocalFiles = false);
+    static Session* instance_;
+};
 
-//    int downloadSpeedLimit() const;
-//    void setDownloadSpeedLimit(int limit);
-//    int uploadSpeedLimit() const;
-//    void setUploadSpeedLimit(int limit);
+} // BitTorrent
 
-//    bool hasActiveTorrents() const;
-//    bool hasUnfinishedTorrents() const;
-//    bool hasRunningSeed() const;
-
-////signals:
-
-//private:
-//    explicit Session(QObject* parent = nullptr);
-//    ~Session();
-
-//    void initResumeFolder();
-
-//    // BitTorrent
-//    libtorrent::session *native_session_;
-
-//    QString resume_folder_path_;
-
-//    SessionStatus status_;
-
-//    static Session *instance_;
-//};
-
-//} // BitTorrent
-//#endif // SESSION_H
+#endif // SESSION_H
